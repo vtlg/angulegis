59b1bf0022c010c424dab8f6a60197defc71b46d
Vinícius Gama <viniciusgama@MacBook-Pro-de-Vinicius.local>
2017-07-04 19:58:54 -0300

    bootstrap configurado

--- angulegis/src/app/app.module.ts
+++ angulegis/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
