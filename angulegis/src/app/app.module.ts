4a8bdb719340bf26fe8d505715831ce1d6aeea99
Vinicius Tavares Lobato Gama <c105118@corp.caixa.gov.br>
2017-07-10 16:15:33 -0300

    navbar instalada

--- angulegis/src/app/app.module.ts
+++ angulegis/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PrincipalComponent
   ],
   imports: [
     NgbModule.forRoot(),
---
+++ angulegis/src/app/principal/principal.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit {
+
+  title = 'app`s Vinícius Tavares Lobato Gama';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
